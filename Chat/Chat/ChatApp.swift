aa39602747d749045da661be1e6e6f82959dc5da
anhtdt <anhtdt@vnpay.vn>
2023-08-11 14:12:20 +0700

    init project

---
+++ Chat/Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Tien Anh Tran Duc on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
